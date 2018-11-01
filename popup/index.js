d4bf497521c1f74f6a07410b651aa92ad36f4676
vlwl李文利 <liwl@ctrip.com>
2018-11-01 17:13:27 +0800

    init

---
+++ popup/index.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+function getLocalIPs(callback) {
+    var ips = [];
+
+    var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+    var pc = new RTCPeerConnection({
+        // Don't specify any stun/turn servers, otherwise you will
+        // also find your public IP addresses.
+        iceServers: []
+    });
+    // Add a media line, this is needed to activate candidate gathering.
+    pc.createDataChannel('');
+
+    // onicecandidate is triggered whenever a candidate has been found.
+    pc.onicecandidate = function (e) {
+        if (!e.candidate) { // Candidate gathering completed.
+            pc.close();
+            callback(ips);
+            return;
+        }
+        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
+        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+            ips.push(ip);
+    };
+    pc.createOffer(function (sdp) {
+        pc.setLocalDescription(sdp);
+    }, function onerror() { });
+}
+
+let cwin = chrome.extension.getBackgroundPage();
+
+$(cwin.document).ready(() => {
+    if (window.__chromePluginIP__) {
+        $('#foo').val(window.__chromePluginIP__);
+    } else {
+        getLocalIPs(function (ips) {
+            let ip = ips[0] || '。。。';
+    
+            window.__chromePluginIP__ = ip;
+        
+            $('#foo').val(ip);
+        });
+    }
+
+    var clipboard = new ClipboardJS('.btn');
+
+    clipboard.on('success', function (e) {
+        $('#resultTxt').text('copy success')
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+        $('#resultTxt').text('copy error')
+    });
+
+})
+
